f17cf3cee41b8a3a011cbca2c3cec0354bab22bf
ajisahputra78 <ajisaputro.s.kom@gmail.com>
2020-10-31 23:13:54 +0700

    Initial Commit

---
+++ UniversityInYogyakrta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversityInYogyakrta
+//
+//  Created by Aji_sahputra on 31/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
